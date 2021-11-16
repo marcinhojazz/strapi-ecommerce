391a3a740fab8d16ae08ba49f34883179cdc6923
Marcio Machado Pontes <marcinhojazz@gmail.com>
2021-11-16 01:40:26 -0300

    create custom pallete colors won

--- admin/src/components/LeftMenu/LeftMenuHeader/Wrapper.js
+++ admin/src/components/LeftMenu/LeftMenuHeader/Wrapper.js
@@ -1,51 +1,20 @@
-import styled from 'styled-components';
-import PropTypes from 'prop-types';
+import styled, { css } from "styled-components";
 
-import Logo from '../../../assets/images/logo-won.svg';
+import Logo from "../../../assets/images/logo-won.svg";
 
 const Wrapper = styled.div`
-  background-color: ${(props) =>
-    props.theme.main.colors.leftMenu['background-header-link']};
-  padding-left: 2rem;
-  height: ${props => props.theme.main.sizes.leftMenu.height};
-
-  .leftMenuHeaderLink {
-    &:hover {
-      text-decoration: none;
+  ${({ theme }) => css`
+    background-color: ${theme.main.colors.won.blue};
+    height: ${theme.main.sizes.leftMenu.height};
+    .projectName {
+      display: block;
+      height: ${theme.main.sizes.leftMenu.height};
+      background-image: url(${Logo});
+      background-repeat: no-repeat;
+      background-position: center center;
+      background-size: 12rem;
     }
-  }
-
-  .projectName {
-    display: block;
-    width: 100%;
-    height: ${props => props.theme.main.sizes.leftMenu.height};
-    font-size: 2rem;
-    letter-spacing: 0.2rem;
-    color: $white;
-
-    background-image: url(${Logo});
-    background-repeat: no-repeat;
-    background-position: left center;
-    background-size: auto 2.5rem;
-  }
+  `}
 `;
-
-Wrapper.defaultProps = {
-  theme: {
-    main: {
-      colors: {
-        leftMenu: {},
-      },
-      sizes: {
-        header: {},
-        leftMenu: {},
-      },
-    },
-  },
-};
-
-Wrapper.propTypes = {
-  theme: PropTypes.object,
-};
 
 export default Wrapper;
