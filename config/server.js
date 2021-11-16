6c54c6e90ec4ffcf7b66f87116640874af594136
Marcio Machado Pontes <marcinhojazz@gmail.com>
2021-11-15 21:23:59 -0300

    init project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7d94ed560629ac7f0b25f0eb042a692f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '286d8b72-8295-4c09-be41-4bde9ebc368d'
+};
